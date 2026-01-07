d71e6f12f161e1e93e84b609117772bef27583ff
rohitar2002 <arora.rohit2408@gmail.com>
2026-01-08 00:21:50 +0530

    env file added again

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
  */
 const nextConfig = {
   reactStrictMode: true, // Recommended for catching issues early
+  env: {
+    APP_FIREBASE_API_KEY: process.env.APP_FIREBASE_API_KEY,
+    APP_FIREBASE_AUTHDOMAIN: process.env.APP_FIREBASE_AUTHDOMAIN,
+    APP_FIREBASE_PROJECTID: process.env.APP_FIREBASE_PROJECTID,
+    APP_FIREBASE_STORAGE_BUCKET: process.env.APP_FIREBASE_STORAGE_BUCKET,
+    APP_FIREBASE_MESSAGE_SENDER_ID: process.env.APP_FIREBASE_MESSAGE_SENDER_ID,
+    APP_FIREBASE_APP_ID: process.env.APP_FIREBASE_APP_ID,
+  }
 };
 
 module.exports = nextConfig;
